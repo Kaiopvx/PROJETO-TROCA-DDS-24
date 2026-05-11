fc19a8804e84dca1b672177ae55af5f4c4707365
Gregory Veloso <gregory.v.silva@aluno.senai.br>
2026-05-11 16:43:23 -0300

    EJS configurado

--- server/server.js
+++ server/server.js
@@ -6,13 +6,30 @@ const path = require("path");
 // Define a porta do servidor
 const port = 3000;
 
+// CONFIGURAÇÃO DO EJS E PASTAS DO FRONT END
+// Define o EJS como engine do front
+app.set("view engine", "ejs");
+// Aponta para o express e ejs onde estão as páginas
+app.set("views", path.join(__dirname, "../client/views"));
+// Deixa a pasta public acessível ao usuário
+app.set(express.static(path.join(__dirname, "../client/public")));
+
 // ROTAS PÚBLICAS
 // Criação de rotas padrão
-app.get("/", (req, res) => { res.status(200).json({ mensagem: "Olá, seja bem vindo" });});
+app.get("/", (req, res) => {
+  // Redireciona pra tela de login
+  res.status(200).redirect("/login");
+});
+
 //Rota que retorna a página de login
-app.get("/login", (req, res) => {res.sendFile(path.join(__dirname, "../client/views/auth/login.html"));});
+app.get("/login", (req, res) => {
+  res.render('auth/login');
+});
+
 // Rota que retorna a página de cadastro de usuário
-app.get("/cadastro", (req, res) => {res.sendFile(path.join(__dirname, "../client/views/auth/cadastro.html"));});
+app.get("/cadastro", (req, res) => {
+  res.render('auth/cadastro');
+});
 
 //Importar as rotas de usuário
 const usuariosRoutes = require("./routes/usuarioRoutes.js");
